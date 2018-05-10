6f63d06b983096404a5d61dbf83e34ecb4f5cdb7
valentindubois <valentin.dubois2@he-arc.ch>
2018-05-10 09:15:08 +0200

    refactoring

---
+++ src/main/java/ch/hearc/st/app/GrilleADFGVX.java
@@ -0,0 +1,98 @@
+package ch.hearc.st.app;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class GrilleADFGVX {
+
+  private String alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+  private String example = "c1ofwjymt5b4i7a28sp30qhxkeul6dvrgzn9";
+
+  String[] lettres = {"A","D","F","G","V","X"};
+
+  private Map<Character, String> grille;
+  private Map<String, Character> grilleInversee;
+
+  public GrilleADFGVX() {
+
+    this.grille = new HashMap<>();
+    this.grilleInversee = new HashMap<>();
+
+  }
+
+  public void create(Boolean random){
+
+    LinkedList<Character> chars = new LinkedList<>();
+
+    if(random){
+      for(Character c : alphabet.toCharArray()) {
+        chars.add(c);
+      }
+      Collections.shuffle(chars);
+    }else{
+      for(Character c : example.toCharArray()) {
+        chars.add(c);
+      }
+    }
+    for(String ligne : lettres){
+      for(String colonne : lettres) {
+        this.grille.put(chars.getFirst(), ligne+colonne);
+        this.grilleInversee.put(ligne+colonne, chars.pop());
+      }
+    }
+  }
+
+  public String print(){
+
+    StringBuilder sb = new StringBuilder();
+    sb.append("   ");
+    for(String colonne : lettres){
+      sb.append(colonne).append("  ");
+    }
+    sb.append("\n");
+    for(String ligne : lettres){
+      sb.append(ligne).append("  ");
+      for(String colonne : lettres) {
+        grille.get(ligne+colonne);
+        sb.append(grilleInversee.get(ligne+colonne)).append("  ");
+      }
+      sb.append("\n");
+    }
+    return sb.toString();
+  }
+
+  public String chiffrer(String message){
+
+    StringBuilder sb = new StringBuilder();
+
+    for(Character c : message.toCharArray()) {
+      sb.append(grille.get(c));
+    }
+
+    return sb.toString();
+  }
+
+  public String dechiffrer(String message){
+
+    StringBuilder sb = new StringBuilder();
+
+    int i = 1;
+    String couple = "";
+
+    for(Character c : message.toCharArray()) {
+      if(i%2==0) {
+        couple = couple + c.toString();
+        sb.append(grilleInversee.get(couple));
+        couple = "";
+      }else{
+        couple=c.toString();
+      }
+      i++;
+    }
+
+    return sb.toString();
+  }
+
+}
--- src/main/java/ch/hearc/st/app/Main.java
+++ src/main/java/ch/hearc/st/app/Main.java
@@ -1,131 +1,20 @@
 package ch.hearc.st.app;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-
 public class Main {
 
   public static void main(String[] args) {
 
-    String alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
-    String example = "c1ofwjymt5b4i7a28sp30qhxkeul6dvrgzn9";
+  GrilleADFGVX g = new GrilleADFGVX();
 
-    /**
-    LinkedList<Character> chars = new LinkedList<>();
-    for(Character c : alphabet.toCharArray()) {
-      chars.add(c);
-    }
-    Collections.shuffle(chars);
-    **/
+  g.create(false);
+  System.out.println(g.print());
 
-    LinkedList<Character> chars = new LinkedList<>();
-    for(Character c : example.toCharArray()) {
-      chars.add(c);
-    }
+  String message = "objectifarras15h";
+  String messageChiffre = g.chiffrer(message);
 
-  Map<Character, String> carte = new HashMap<>();
-  Map<String, Character> carteInversee = new HashMap<>();
-
-  String[] m = {"A","D","F","G","V","X"};
-  for(String ligne : m){
-    for(String colonne : m) {
-      carte.put(chars.getFirst(), ligne+colonne);
-      carteInversee.put(ligne+colonne, chars.pop());
-    }
-  }
-
-  String messageClair = "objectifarras15hobjectifarras15h";
-    System.out.println(messageClair);
-
-  StringBuilder sb = new StringBuilder();
-    for(Character c : messageClair.toCharArray()) {
-      sb.append(carte.get(c));
-    }
-  String messageChiffre = sb.toString();
     System.out.println(messageChiffre);
 
-    //print
-    sb = new StringBuilder();
-    sb.append("   ");
-    for(String colonne : m){
-      sb.append(colonne).append("  ");
-    }
-    System.out.println(sb.toString());
-    for(String ligne : m){
-      sb = new StringBuilder();
-      sb.append(ligne).append("  ");
-      for(String colonne : m) {
-        carteInversee.get(ligne+colonne);
-        sb.append(carteInversee.get(ligne+colonne)).append("  ");
-      }
-      System.out.println(sb.toString());
-    }
-
-
-
-    String code = "DEMAIN";
-
-
-
-    int ii = 0;
-    int jj = 0;
-    for(Character c : messageChiffre.toCharArray()) {
-      jj++;
-      if(jj==code.length()){
-        jj=0;
-        ii++;
-      }
-    }
-
-    int len = ii+1;
-
-    //générer grille 1
-    String[][] grille = new String[len][code.length()];
-    int i = 0;
-    int j = 0;
-
-
-    /**
-    for(Character c : messageChiffre.toCharArray()) {
-
-      grille[i][j] = c.toString();
-      j++;
-
-      if(j==code.length()){
-        j=0;
-        i++;
-      }
-
-    }**/
-
-
-    int count = 0;
-
-    for(i=0;i<grille.length;i++){
-
-      for(j=0;j<grille[i].length;j++){
-        if(count<messageChiffre.length()) {
-          Character cc = messageChiffre.charAt(count);
-          grille[i][j] = cc.toString();
-        }else{
-          grille[i][j] = "";
-        }
-        count++;
-      }
-
-    }
-
-
-    System.out.println("grille1");
-    for(i=0;i<grille.length;i++){
-      sb = new StringBuilder();
-      for(j=0;j<grille[i].length;j++){
-        sb.append(grille[i][j]).append(" ");
-      }
-      System.out.println(sb.toString());
-    }
-
+    System.out.println(g.dechiffrer(messageChiffre));
 
 
   }
