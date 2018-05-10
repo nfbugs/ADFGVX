f16fdcbc87d03867e639fd365e1a08b4c15a22b6
Hugo Gonçalves <hugo.goncalves@he-arc.ch>
2018-05-10 16:54:04 +0200

    creerPermut ok TestClePermutation added to test the method

---
+++ src/main/java/ch/hearc/st/app/TestClePermutation.java
@@ -0,0 +1,12 @@
+package ch.hearc.st.app;
+
+public class TestClePermutation {
+
+  public static void main(String[] args) {
+    Crypto crypto = new Crypto();
+    String entree = "chat";
+
+    System.out.println(crypto.creerPermut(entree));
+  }
+
+}
